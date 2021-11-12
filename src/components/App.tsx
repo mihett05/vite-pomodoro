53384a7661867ef784a1fbe4a80789e4d6d18b2c
mihett05 <mihett05@yandex.ru>
2021-11-12 20:15:54 +0500

    Init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        Pomodoro clock
+      </div>
+    </div>
+  )
+}
+
+export default App
